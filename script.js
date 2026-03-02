37602879d9a2dfd4514933bff13430d7fa307e09
ChrissyTRX <cdriverac.7@gmail.com>
2026-03-01 18:18:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -128,4 +128,17 @@ document.addEventListener("DOMContentLoaded", function () {
         doc.save("Curriculum.pdf");
     });
 
+
+});
+// ===== MODO OSCURO =====
+const toggleBtn = document.getElementById("darkModeToggle");
+
+toggleBtn.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        toggleBtn.textContent = "☀️";
+    } else {
+        toggleBtn.textContent = "🌙";
+    }
 });
